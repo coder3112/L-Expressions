4edba9a581867546b1814034973ee0bc70d75a2f
Naitik Mundra <mundranaitik@outlook.com>
2025-06-28 12:23:41 +0200

    saving in gc to delete. I need a structure where I can ensure that A only has 1 child etc. this is not that. but useful to have as ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello there!")
+    let tree = NodeStruct {
+        value: Node::Symbol(Symbol::S),
+        children: Some(vec![
+            NodeStruct {
+                value: Node::Symbol(Symbol::Z),
+                children: None,
+            },
+            NodeStruct {
+                value: Node::Symbol(Symbol::A),
+                children: None,
+            },
+            NodeStruct {
+                value: Node::Nat { value: 0 },
+                children: None,
+            },
+        ]),
+    };
+
+    println!("{:#?}", tree);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Symbol {
+    A,
+    Z,
+    S,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Node {
+    Nat { value: u64 },
+    Symbol(Symbol),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct NodeStruct {
+    value: Node,
+    children: Option<Vec<NodeStruct>>,
 }
